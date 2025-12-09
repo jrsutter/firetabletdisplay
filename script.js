f8b19319d7f9687769e8948cb8a6f0cfd6996f81
jrsutter <jarrettsutter@gmail.com>
2025-12-09 16:56:16 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // === CONFIG ===
-console.log("Script loaded");
 const API_KEY = "ccd59c5e8afff546aeb07513036a9b55"; // replace with your API key
 const LAT = "30.312156";
 const LON = "-95.456014";
